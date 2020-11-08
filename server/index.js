8afa24690efa01a7f6c6c22c9faa9068d7915341
ddmy <841312998@qq.com>
2020-11-08 18:06:28 +0800

    配置 server 构建命令

--- server/index.js
+++ server/index.js
@@ -1,26 +1,32 @@
-// const express = require('express')
+const express = require('express')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackDevServer = require('webpack-dev-server')
 
-// const app = express()
-const config = require('../webpack.config')
+const IS_DEV = process.env.NODE_ENV === 'development'
+let server
+let config
 
-const options = {
-  contentBase: './dist',
-  hot: true,
-  host: 'localhost',
-  open: true
+if (IS_DEV) {
+  config = require('../webpack.dev.js')
+  const options = {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost',
+    open: true
+  }
+  webpackDevServer.addDevServerEntrypoints(config, options)
+  const compiler = webpack(config)
+  server = new webpackDevServer(compiler, options)
+} else {
+  server = express()
+  config = require('../webpack.prod.js')
+  const compiler = webpack(config)
+  server.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  }))
 }
-webpackDevServer.addDevServerEntrypoints(config, options)
-
-const compiler = webpack(config)
-const server = new webpackDevServer(compiler, options)
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: config.output.publicPath
-// }))
-
 
 server.listen(3333, function () {
   console.log('Example app listening on port 3000!\n');
-});
+})
